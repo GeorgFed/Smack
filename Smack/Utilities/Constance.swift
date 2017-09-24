88d3fe768737cf39ca56da3ac681af6a71bbf204
GeorgFed <azurit78@gmail.com>
2017-09-24 13:14:49 +0300

    working on API with mongodb and heroku

--- Smack/Utilities/Constance.swift
+++ Smack/Utilities/Constance.swift
@@ -7,6 +7,18 @@
 //
 
 import Foundation
+
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// MARK: Conatants
+let BASE_URL = "https://chatforsmack.herokuapp.com/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+// MARK: Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// MARK: User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN = "loggedIn"
+let USER_EMAIL = "userEmail"
