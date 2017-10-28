9fb5559511c53b83c87c32fe38f7ee3451e685a7
GeorgFed <azurit78@gmail.com>
2017-10-29 00:43:18 +0300

    Added login service and universal login, so that even after a user closes the app, he or she can open it later and they are still logged in

--- Smack/Utilities/Constance.swift
+++ Smack/Utilities/Constance.swift
@@ -15,6 +15,7 @@ let BASE_URL = "https://badguysgang.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
+let URL_USER_BY_EMAIL = "\(BASE_URL)user/byEmail/"
 // MARK: Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
@@ -34,5 +35,9 @@ let HEADER = [
     "Content-Type" : "application/json; charset=utf-8"
 ]
 
+let BEARER_HEADER = [
+    "Authorization" : "Bearer \(AuthService.instance.authToken)",
+    "Content" : "application/json; charset=utf-8"
+]
 // MARK: Colors
 let SMACK_PLACEHOLDER = #colorLiteral(red: 1, green: 0.4932718873, blue: 0.4739984274, alpha: 0.5016415836)
